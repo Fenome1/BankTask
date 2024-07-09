4bf4fa29c38f1e273d218d654027ae9d931852a6
Fenome1 <warfaceserii@mail.ru>
2024-07-09 18:30:24 +0300

    fix docker

--- bank.back/Bank.Persistence/Context/BankDbContext.cs
+++ bank.back/Bank.Persistence/Context/BankDbContext.cs
@@ -26,10 +26,6 @@ public partial class BankDbContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https: //go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=bank;Username=postgres;Password=P@ssw0rd;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Account>(entity =>
